45eabf8e741c84d510c396b306eb46de8f36c4af
Javier Martín <elretirao@elretirao.net>
2010-08-10 18:30:02 +0200

    Se nos había olvidado incluir el esquema de la BD. ¡db:migrate!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100810160344) do
+ActiveRecord::Schema.define(:version => 20100810162021) do
+
+  create_table "cajas", :force => true do |t|
+    t.string   "titulo"
+    t.text     "cuerpo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "paginas", :force => true do |t|
     t.string   "titulo"
