b1b982558f6445a6635c17ba4a110f1ded098b6c
Javier Martín <elretirao@elretirao.net>
2010-08-10 18:58:20 +0200

    La página principal es el listado de páginas.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Calesur::Application.routes.draw do
 
   resources :paginas
 
+  root :to => "paginas#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,10 +52,6 @@ Calesur::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
