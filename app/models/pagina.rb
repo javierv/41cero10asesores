110b34b4350b3cc75849bcc3b49a61406762530f
Javier Martín <elretirao@elretirao.net>
2010-08-10 21:36:32 +0200

    Función para ordenar las cajas de cada página.

--- app/models/pagina.rb
+++ app/models/pagina.rb
@@ -6,4 +6,10 @@ class Pagina < ActiveRecord::Base
 
   has_many :sidebars
   has_many :cajas, :through => :sidebars
+
+  def build_sidebar(caja_ids)
+    caja_ids.each_with_index do |caja_id, index|
+      sidebars.build(:caja_id => caja_id, :orden => index + 1)
+    end
+  end
 end
