6c2512174a4d171f9d4ceb15d9af819703010052
Javier Martín <elretirao@elretirao.net>
2010-08-10 18:16:48 +0200

    Corregimos tabulación.

--- lib/templates/shoulda/model/model.rb
+++ lib/templates/shoulda/model/model.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class <%= class_name %>Test < ActiveSupport::TestCase
   <%- for attribute in attributes -%>
-    should validate_presence_of(:<%= attribute.name %>)
-    should allow_mass_assignment_of(:<%= attribute.name %>)
+  should validate_presence_of(:<%= attribute.name %>)
+  should allow_mass_assignment_of(:<%= attribute.name %>)
   <%- end -%>
 end
