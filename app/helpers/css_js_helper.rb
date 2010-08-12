d4b33c58c5096cb1b6687faae6dd7c1b4af51761
Javier Martín <elretirao@elretirao.net>
2010-08-12 19:09:44 +0200

    Uso de bsmselect para manejar las cajas.

--- app/helpers/css_js_helper.rb
+++ app/helpers/css_js_helper.rb
@@ -10,4 +10,14 @@ module CssJsHelper
       '/javascripts/markitup/sets/textile/style.css'
     javascript 'markitup/jquery.markitup', 'markitup/sets/textile/set'
   end
+
+  def bsmselect
+    css 'jquery.bsmselect'
+    javascript 'jquery.bsmselect'
+  end
+
+  def bsmselect_con_cajas
+    bsmselect
+    javascript 'cajas'
+  end
 end
