f380b975c3e2e3a023bb349e3c319f8d8badb95e
Javier Martín <elretirao@elretirao.net>
2010-08-16 18:39:11 +0200

    Una navegación temporal, puesta "a mano".

---
+++ app/helpers/navegacion_helper.rb
@@ -0,0 +1,18 @@
+module NavegacionHelper
+  def lista_con_enlaces(enlaces, opciones = {})
+    return '' if enlaces.empty?
+    content_tag :ul, opciones do
+      enlaces.inject('') do |contenido, enlace|
+        contenido + elemento_lista_enlace(enlace)
+      end
+    end
+  end
+  
+private
+  def elemento_lista_enlace(enlace)
+    opciones = enlace.extract_options!
+    content_tag :li do
+      link_to enlace[0], enlace[1], opciones
+    end
+  end
+end
