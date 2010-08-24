dc9ce1d5cf880262cfb594d065d9e136cd5daf93
Javier Martín <elretirao@elretirao.net>
2010-08-24 21:44:51 +0200

    attr_writer para simplificar código.

--- app/models/pagina.rb
+++ app/models/pagina.rb
@@ -1,5 +1,6 @@
 class Pagina < ActiveRecord::Base
   attr_accessible :titulo, :cuerpo
+  attr_writer :ids_cajas
   validates :titulo, :presence => true
   validates :cuerpo, :presence => true
   display_name :titulo
@@ -8,15 +9,11 @@ class Pagina < ActiveRecord::Base
   has_many :cajas, :through => :sidebars
   before_save :build_sidebar
 
-  def ids_cajas=(caja_ids)
-    @caja_ids = caja_ids
-  end
-
 private
   def build_sidebar
-    if @caja_ids
+    if @ids_cajas
       sidebars.destroy_all
-      @caja_ids.reject {|caja_id| caja_id.to_i.zero?}.each_with_index do |caja_id, index|
+      @ids_cajas.reject {|caja_id| caja_id.to_i.zero?}.each_with_index do |caja_id, index|
         sidebars.build(:caja_id => caja_id, :orden => index + 1)
       end
     end
