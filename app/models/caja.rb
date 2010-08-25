610f6607d621801e70cd4d957d55caf60f78e67d
Javier Martín <elretirao@elretirao.net>
2010-08-25 21:23:48 +0200

    Al editar una caja, mostramos las cajas seleccionadas en el mismo orden en que se guardaron.

--- app/models/caja.rb
+++ app/models/caja.rb
@@ -5,4 +5,14 @@ class Caja < ActiveRecord::Base
   display_name :titulo
 
   has_many :sidebars
+  scope :por_titulo, order("cajas.titulo")
+
+  scope :al_final_las_de_pagina, lambda {|pagina|
+    if pagina.new_record?
+      por_titulo
+    else
+      joins("LEFT JOIN sidebars ON sidebars.caja_id=cajas.id AND pagina_id = #{pagina.id}").
+      order("sidebars.orden, cajas.titulo")
+    end
+  }
 end
