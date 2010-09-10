58ef895f6cc5720353646036918feb165998605e
Javier Martín <elretirao@elretirao.net>
2010-09-11 00:08:25 +0200

    Previsualización por AJAX cada vez que se teclea algo y se deja de teclear.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -6,10 +6,21 @@ $(document).ready(function()
 
   $('input[name=preview]').click(function()
   {
-    $(this).parents('form').ajaxSubmit({
+    preview();
+    return false;
+  })
+  
+  $('input[type=text], textarea', $('form.simple_form')).typeWatch({
+    callback: function() {preview()},
+    wait: 1000,
+    captureLenght: 1
+  });
+
+  function preview()
+  {
+    $('form.simple_form').ajaxSubmit({
       target: '#preview',
       data: { preview: true }
     });
-    return false;
-  })
+  }
 });
