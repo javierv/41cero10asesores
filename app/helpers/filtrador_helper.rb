0b39bcc1f713f5ddadfce733e71ca30cf24a1141
Javier Martín <elretirao@elretirao.net>
2010-11-08 17:52:25 +0100

    Más flexibilidad en el filtrador: permitimos usar un bloque para los campos (no siempre vamos a querer campos de texto).

---
+++ app/helpers/filtrador_helper.rb
@@ -0,0 +1,22 @@
+module FiltradorHelper
+  def filter_form_for(search, &block)
+    content_tag :section, :id => 'filtrador' do
+      filter_form(search, &block) + reset_button(search)
+    end
+  end
+
+private
+  def filter_form(search, &block)
+    simple_form_for search do |form|
+      block.call(form)
+      form.submit 'Filtrar'
+    end
+  end
+
+  def reset_button(search)
+    simple_form_for search, :html => {:id => 'reset_search', :class => 'reset'} do |form|
+      hidden_field_tag :reset, true
+      form.submit 'Ver todos', :id => 'reset_button'
+    end
+  end
+end
