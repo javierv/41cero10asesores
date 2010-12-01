1fda6d1f6e7b37cb266d0f08c6e28f84f913c0e3
Javier Martín <elretirao@elretirao.net>
2010-12-01 21:42:00 +0100

    Añadido ayudante para generar tiempos en HTML5.
    Sacado de:
    
    https://rails.lighthouseapp.com/projects/8994/tickets/5919-helper-for-html5-time-tag
    
    Cuando entre en Rails, habrá que quitarlo de aquí.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       "Editando la página #{pagina}"
     end
   end
+
+
+  def time_tag(date_or_time, *args)
+    options  = args.extract_options!
+    content  = args.first || I18n.l(date_or_time, :format => :long)
+    datetime = date_or_time.acts_like?(:time) ? date_or_time.xmlschema : date_or_time.rfc3339
+    content_tag :time, content, options.reverse_merge(:datetime => datetime)
+  end
 end
