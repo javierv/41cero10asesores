28167ec5d49ebb144f94d7000741f31c7a916a9d
Javier Martín <elretirao@elretirao.net>
2010-12-03 00:59:45 +0100

    A falta de más datos de usuario, le mostramos como su correo electrónico.

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,5 @@
 class Usuario < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  display_name :email
 end
