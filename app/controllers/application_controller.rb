38fe44b8eb039747094508ec9f6991850b1c5661
Javier Martín <elretirao@elretirao.net>
2010-12-03 01:05:52 +0100

    Conservamos los parámetros de filtros, página y orden en los índices.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :conservar_parametros, :only => [:index]
+
+private
+  def conservar_parametros
+    session_key = :"#{params[:controller]}.#{params[:action]}"
+
+    if estan_accediendo_al_indice_principal?
+      self.params = session[session_key] if session[session_key]
+    else
+      session[session_key] = params
+    end
+  end
+
+  def estan_accediendo_al_indice_principal?
+    params.reject{|key, value| [:controller, :action].include?(key.to_sym)}.empty?
+  end
 end
