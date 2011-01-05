7a98102d1c0ed82b6ed1ef9ae59ea7f5283c3745
Javier Martín <elretirao@elretirao.net>
2011-01-05 02:20:48 +0100

    Sacamos de control de versiones los archivos compilados por coffeescript.

--- public/javascripts/form.js
+++
@@ -1,9 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Wed, 05 Jan 2011 00:39:47 GMT from
- * /home/javi/programas/work/calesur/app/javascripts/form.coffee
- */
-
-(function() {
-  $(document).ready(function() {
-    return $('textarea').markItUp(markitup_settings);
-  });
-}).call(this);
