3878c698be039cc44e86fa51bbb415b56f842caf
Javier Martín <elretirao@elretirao.net>
2011-01-05 20:45:41 +0100

    Incluimos una zona con las acciones de administración si el usuario está identificado.

--- app/helpers/navegacion_helper.rb
+++ app/helpers/navegacion_helper.rb
@@ -7,10 +7,23 @@ module NavegacionHelper
       end
     end
   end
+
+  def navegacion_admin    
+    lista_con_enlaces [['Editar páginas', paginas_path, {:controller => 'paginas'}],
+     ['Editar cajas', cajas_path, {:controller => 'cajas'}],
+     ['Editar navegación', new_navegacion_path, {:controller => 'navegaciones'}],
+     ['Desconectar', destroy_usuario_session_path]]
+  end
   
 private
   def elemento_lista_enlace(enlace)
     opciones = enlace.extract_options!
+
+    if opciones[:controller] == params[:controller]
+      opciones[:class] = 'current'
+      opciones.delete :controller
+    end
+
     content_tag :li do
       link_to enlace[0], enlace[1], opciones
     end
