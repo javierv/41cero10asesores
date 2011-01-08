5ef8a570fd549e858ec60fae078fe7ce293047e5
Javier Martín <elretirao@elretirao.net>
2011-01-08 22:38:45 +0100

    TODO añadido.

--- app/controllers/ajax_form_controller.rb
+++ app/controllers/ajax_form_controller.rb
@@ -2,6 +2,7 @@ class AjaxFormController < ApplicationController
   respond_to :js
 
   def autocomplete
+    #TODO: mover al modelo
     modelo = params[:url].split('/')[1].singularize.classify.constantize
     campo_busqueda = /\[(.*)\]/.match(params[:name])[1]
     campo = campo_busqueda.sub('_contains', '').to_sym
