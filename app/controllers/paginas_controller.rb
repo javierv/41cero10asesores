eb2da364040dbeb80e1655e10ba1982231bd98ab
Javier Martín <elretirao@elretirao.net>
2011-01-17 17:56:39 +0100

    Corregimos el problema de respond_with en Firefox y Chromium.
    La pena es que en rekonk sigue fallando...

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class PaginasController < ApplicationController
+  # Tengo que declarar antes el JS para que las peticiones AJAX respondan así.
+  respond_to :js, :only => [:index]
   respond_to :html
 
   before_filter :find_pagina, :only => [:show, :edit, :update, :destroy, :historial]
@@ -14,13 +16,7 @@ class PaginasController < ApplicationController
     @search = Pagina.metasearch params[:search]
     @paginas = @search.where(:published_id => nil).paginate :page => params[:page],
       :per_page => Pagina.per_page
-
-    # Me pasa lo mismo que con preview. Sigo sin saber por qué.
-    if request.xhr?
-      render 'index.js'
-    else
-      respond_with @paginas
-    end
+    respond_with @paginas    
   end
 
   def show
