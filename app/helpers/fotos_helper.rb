e32052206882bab7d8782f87de1ef30d691ed532
Javier Martín <elretirao@elretirao.net>
2011-01-18 16:31:29 +0100

    No mostramos la imagen original, sino que la redimensionamos.

--- app/helpers/fotos_helper.rb
+++ app/helpers/fotos_helper.rb
@@ -1,5 +1,5 @@
 module FotosHelper
   def thumbnail(foto)
-    link_to image_tag(foto.imagen.thumb('100x100#').url), foto.imagen.url
+    link_to image_tag(foto.imagen.thumb('100x100#').url), foto.imagen.thumb('200x200>').url
   end
 end
