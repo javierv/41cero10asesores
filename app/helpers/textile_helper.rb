024bbd4b889db5478db99e413b15cffc6c28484c
Javier Martín <elretirao@elretirao.net>
2011-01-19 17:24:14 +0100

    Permitimos al usuario definir el estilo, que en principio no tendría que ser demasiado ofensivo (text-align o similar) ni peligroso.

--- app/helpers/textile_helper.rb
+++ app/helpers/textile_helper.rb
@@ -2,6 +2,6 @@ module TextileHelper
   def strict_textilize(texto)
    sanitize textilize(texto),
       :tags       => %w(a acronym strong em li ul ol blockquote br cite sub sup ins p img),
-      :attributes => %w(href src alt class)
+      :attributes => %w(href src alt class style)
   end
 end
