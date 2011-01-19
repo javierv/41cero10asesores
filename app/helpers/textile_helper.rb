ada231530be2954eaa1a7876825fb733a3f640b5
Javier Martín <elretirao@elretirao.net>
2011-01-19 22:57:26 +0100

    Permitimos la etiqueta que tacha el texto.

--- app/helpers/textile_helper.rb
+++ app/helpers/textile_helper.rb
@@ -1,7 +1,7 @@
 module TextileHelper
   def strict_textilize(texto)
    sanitize textilize(texto),
-      :tags       => %w(a acronym strong em li ul ol blockquote br cite sub sup ins p img h2),
+      :tags       => %w(a acronym strong em li ul ol blockquote br cite sub sup ins p img h2 del),
       :attributes => %w(href src alt class style)
   end
 end
