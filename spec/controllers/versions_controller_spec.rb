f819e1e27c75934d4fdc4e219a6ef22895b844c4
Javier Martín <elretirao@elretirao.net>
2011-02-03 22:27:39 +0100

    Resto de tests de controladores pasan.

--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -1,69 +1,70 @@
 # encoding: utf-8
 
-require 'test_helper'
+require 'spec_helper'
 
-class VersionsControllerTest < ActionController::TestCase
+describe VersionsController do
+  before(:each) do
+    authenticate_usuario
+  end
+
   def pagina_con_versiones
     pagina = Factory(:pagina, :titulo => 'Título previo')
     pagina.update_attribute(:titulo, 'Título final')
     pagina
   end
 
-  should route(:get, "/versions/1/compare/2").
-          to(:action => :compare, :id => 1, :ref_id => 2)
-
   context 'show action' do
-    setup do
+    before(:each) do
       @pagina = pagina_con_versiones
       get :show, :id => @pagina.versions.last.to_param
     end
     
-    should assign_to(:pagina)
-    should respond_with(:success)
+    it { should assign_to(:pagina) }
+    it { should respond_with(:success) }
   end    
 
   context 'recover action' do
-    setup do
+    before(:each) do
       @pagina = pagina_con_versiones
       put :recover, :id => @pagina.versions.last.to_param
     end
-    should assign_to(:pagina)
-    should set_the_flash
-    should redirect_to("la página recuperada") {@pagina}
+    it { should assign_to(:pagina) }
+    it { should set_the_flash }
+    it { should redirect_to(@pagina) }
   end
 
   context 'restore action' do
-    setup do
+    before(:each) do
       @pagina = pagina_con_versiones
       @pagina.destroy
       put :restore, :id => @pagina.versions.last.to_param
     end
-    should set_the_flash
-    should redirect_to("listado de páginas") {paginas_path}
+    it { should set_the_flash }
+    it { should redirect_to(paginas_path) }
   end
 
   context 'compare action' do
-    setup do
+    before(:each) do
       @pagina = pagina_con_versiones
     end
 
     context 'sin versión de referencia' do
-      setup do
+      before(:each) do
         get :compare, :id => @pagina.versions.last.to_param
       end
 
-      should respond_with(:success)
-      should assign_to(:pagina)
-      should assign_to(:referencia)
+      it { should respond_with(:success) }
+      it { should assign_to(:pagina) }
+      it { should assign_to(:referencia) }
     end
 
     context 'con versión de referencia' do
-      setup do
+      before(:each) do
         get :compare, :id => @pagina.versions.last.to_param,
             :ref_id => @pagina.versions.first.to_param
       end
 
-      should respond_with(:success)
+      it { should respond_with(:success) }
     end
   end
 end
