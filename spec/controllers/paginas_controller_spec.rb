694808d54ad01aa4ddf0c9d1c9a483f3826f81be
Javier Martín <elretirao@elretirao.net>
2011-02-03 22:01:09 +0100

    El controlador de páginas se prueba con RSpec.

--- spec/controllers/paginas_controller_spec.rb
+++ spec/controllers/paginas_controller_spec.rb
@@ -1,45 +1,44 @@
 # encoding: utf-8
 
-require 'test_helper'
+require 'spec_helper'
 
-class PaginasControllerTest < ActionController::TestCase
-  tests PaginasController
-
-  setup do
+describe PaginasController do
+  before(:each) do
     @pagina = Factory(:pagina)
+    authenticate_usuario
   end
 
   context "index action" do
-    setup do
+    before(:each) do
       get :index
     end
 
-    should respond_with(:success)
-    should assign_to(:paginas)
-    should render_with_layout(:application)
+    it { should respond_with(:success) }
+    it { should assign_to(:paginas) }
+    it { should render_with_layout(:application) }
   end
 
   context "Índice con AJAX" do
-    setup do
+    before(:each) do
       xhr :get, :index
     end
 
-    should respond_with_content_type(:js)
-    should render_template(:index)
-    should respond_with(:success)
-    should_not render_with_layout
+    it { should respond_with_content_type(:js) }
+    it { should render_template(:index) }
+    it { should respond_with(:success) }
+    it { should_not render_with_layout }
   end
 
   context "new action" do
-    setup do
+    before(:each) do
       get :new
     end
-    should render_template(:new)
+    it { should render_template(:new) }
   end
 
   context "create action" do
     context "when model is invalid" do
-      setup do
+      before(:each) do
         errors = ActiveModel::Errors.new(Pagina.new)
         errors.add_on_blank(:id)
         Pagina.any_instance.stubs(:errors).returns(errors)
@@ -47,57 +46,57 @@ class PaginasControllerTest < ActionController::TestCase
         post :create, :pagina => @pagina.attributes
       end
 
-      should render_template(:new)
+      it { should render_template(:new) }
     end
 
     context "when model is valid" do
-      setup do
+      before(:each) do
         Pagina.any_instance.stubs(:errors).returns({})
         Pagina.any_instance.stubs(:valid?).returns(true)
         post :create, :pagina => @pagina.attributes
       end
-      should redirect_to('') {pagina_path(assigns(:pagina))}
+      it { should redirect_to(pagina_path(assigns(:pagina))) }
     end
 
     context "when using preview button" do
       context "with a normal request" do
-        setup do
+        before(:each) do
           post :create, :pagina => @pagina.attributes, :preview => true
         end
 
-        should render_template(:preview)
-        should assign_to(:pagina)
+        it { should render_template(:preview) }
+        it { should assign_to(:pagina) }
       end
 
       context "with an AJAX request" do
-        setup do
+        before(:each) do
           xhr :post, :create, :pagina => @pagina.attributes, :preview => true
         end
 
-        should render_template(:preview)
-        should respond_with_content_type(:js)
-        should_not render_with_layout
+        it { should render_template(:preview) }
+        it { should respond_with_content_type(:js) }
+        it { should_not render_with_layout }
       end
     end
   end
 
   context "show action" do
-    setup do
+    before(:each) do
       get :show, :id => @pagina.to_param
     end
-    should respond_with(:success)
+    it { should respond_with(:success) }
   end
 
   context "edit action" do
-    setup do
+    before(:each) do
       get :edit, :id => @pagina.to_param
     end
-    should render_template(:edit)
+    it { should render_template(:edit) }
   end
 
   context "update action" do
     context "when model is invalid" do
-      setup do
+      before(:each) do
         errors = ActiveModel::Errors.new(Pagina.new)
         errors.add_on_blank(:id)
         Pagina.any_instance.stubs(:errors).returns(errors)
@@ -105,124 +104,124 @@ class PaginasControllerTest < ActionController::TestCase
         put :update, :id => @pagina.to_param, :pagina => @pagina.attributes
       end
 
-      should render_template(:edit)
+      it { should render_template(:edit) }
     end
 
     context "when model is valid" do
-      setup do
+      before(:each) do
         Pagina.any_instance.stubs(:errors).returns({})
         Pagina.any_instance.stubs(:valid?).returns(true)
         put :update, :id => @pagina.to_param, :pagina => @pagina.attributes
       end
-      should redirect_to('') {pagina_path(@pagina)}
+      it { should redirect_to(pagina_path(@pagina)) }
     end
 
     context "when using preview button" do
       context "with a normal request" do
-        setup do
+        before(:each) do
           put :update, :id => @pagina.to_param, :pagina => @pagina.attributes, :preview => true
         end
 
-        should render_template(:preview)
-        should assign_to(:pagina)
+        it { should render_template(:preview) }
+        it { should assign_to(:pagina) }
       end
 
       context "with an AJAX request" do
-        setup do
+        before(:each) do
           xhr :put, :update, :id => @pagina.to_param, :pagina => @pagina.attributes, :preview => true
         end
 
-        should render_template(:preview)
-        should respond_with_content_type(:js)
-        should_not render_with_layout
+        it { should render_template(:preview) }
+        it { should respond_with_content_type(:js) }
+        it { should_not render_with_layout }
       end
     end
 
     context "when saving as draft" do
       context "with a normal request" do
-        setup do
+        before(:each) do
           put :update, :id => @pagina.to_param, :pagina => @pagina.attributes, :draft => true
         end
 
-        should redirect_to('') {edit_pagina_path(@pagina.draft)}
+        it { should redirect_to(edit_pagina_path(@pagina.draft)) }
       end
 
       context "with an AJAX request" do
-        setup do
+        before(:each) do
           xhr :put, :update, :id => @pagina.to_param, :pagina => @pagina.attributes, :draft => true
         end
 
-        should render_template(:borrador)
-        should respond_with_content_type(:js)
-        should_not render_with_layout
+        it { should render_template(:borrador) }
+        it { should respond_with_content_type(:js) }
+        it { should_not render_with_layout }
       end
     end
 
     context "when publishing a draft" do
-      setup do
+      before(:each) do
         @pagina.save_draft
         @borrador = @pagina.draft
         @action = lambda {put :update, :id => @borrador.to_param, :pagina => @pagina.attributes, :publish => true}
       end
       
       context "when model is valid" do
-        setup do
+        before(:each) do
           Pagina.any_instance.stubs(:valid?).returns(true)
           @action.call
         end
-        should redirect_to('') {pagina_path(@pagina)}
+        it { should redirect_to(pagina_path(@pagina)) }
       end
 
       context 'when model is invalid' do
-        setup do
+        before(:each) do
           Pagina.any_instance.stubs(:valid?).returns(false)
           @action.call
         end
 
-        should render_template(:edit)
+        it { should render_template(:edit) }
       end
     end
   end
 
   context "destroy action" do
     context "with a normal request" do 
-      setup do
+      before(:each) do
         delete :destroy, :id => @pagina.to_param
       end
 
-      should redirect_to('index') { paginas_path }
-      should set_the_flash
-      should "destroy model" do
+      it { should redirect_to(paginas_path) }
+      it { should set_the_flash }
+      it 'destroy model' do
         assert !Pagina.exists?(@pagina.id)
       end
     end
 
     context "with an AJAX request" do
-      setup do
+      before(:each) do
         xhr :delete, :destroy, :id => @pagina.to_param
       end
 
-      should_not render_with_layout
-      should respond_with(:success)
+      it { should_not render_with_layout }
+      it { should respond_with(:success) }
     end
   end
 
   context "search action" do
-    setup do
+    before(:each) do
       get :search, :q => "buscando"
     end
 
-    should respond_with(:success)
-    should assign_to(:paginas)
+    it { should respond_with(:success) }
+    it { should assign_to(:paginas) }
   end
 
   context 'ver el historial' do
-    setup do
+    before(:each) do
       get :historial, :id => @pagina.to_param
     end
 
-    should respond_with(:success)
-    should assign_to(:pagina)
-    should assign_to(:versiones)
+    it { should respond_with(:success) }
+    it { should assign_to(:pagina) }
+    it { should assign_to(:versiones) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,11 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::TestHelpers, :type => :controller
+end
+
+def authenticate_usuario
+  @usuario = Factory :usuario
+  sign_in @usuario
 end
