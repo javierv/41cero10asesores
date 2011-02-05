75b64c967f8113fa90222b23908df7b07016bce7
Javier Martín <elretirao@elretirao.net>
2011-02-05 20:31:51 +0100

    Usamos whenever para actualizar el índice de Xapian periódicamente.

---
+++ config/schedule.rb
@@ -0,0 +1,30 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+every :hour do
+  # TODO: al menos con Passenger parece que hay que reiniciar
+  # el servidor, o al menos si la búsqueda está en caché.
+  rake "xapit:reindex"
+end
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+#
+# every :friday, :at => '10:30 pm'  
+#
+# every :reboot do
+#
+# Learn more: http://github.com/javan/whenever
