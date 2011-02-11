7fab09271fe51887a357e118f2a67472add90380
Javier Martín <elretirao@elretirao.net>
2011-02-11 21:30:11 +0100

    Sólo la última versión de Capybara admite "matchers" de RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
+  require 'capybara/rspec/matchers'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
@@ -37,6 +38,7 @@ Spork.prefork do
     config.use_transactional_fixtures = true
 
     config.include Devise::TestHelpers, :type => :controller
+    config.include Capybara::RSpecMatchers, :type => :helper
   end
 end
 
