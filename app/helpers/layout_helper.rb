fa87cbc7976cea637f3a5872b2cbf590f50cc68e
Javier Martín <elretirao@elretirao.net>
2011-02-13 23:04:52 +0100

    Refactorización del título de layout.
    Nos deshacemos definitivamente del content_for, lo que lleva a un uso
    más limpio. Y además, así se pueden hacer tests más fácilmente.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,9 +1,11 @@
 # encoding: utf-8
 
 module LayoutHelper
-  def title(page_title, show_title = true)
-    content_for(:title) { page_title.to_s }
-    @show_title = show_title
+  # Compatible with ryanb's
+  # It doesn't mess up with content_for, but the code is quite ugly
+  def title(page_title = @page_title, show_title = true)
+    @show_title = show_title unless defined?(@show_title)
+    @page_title = page_title
   end
 
   def show_title?
--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -86,4 +86,42 @@ describe LayoutHelper do
       end
     end
   end
+
+  describe "#title" do
+    let(:title) { 'Mi título' }
+
+    context "with no arguments" do
+      before(:each) do
+        helper.title title
+      end
+
+      it "sets the title" do
+        helper.title.should == title
+      end
+
+      it "sets it shows the title" do
+        helper.show_title?.should be_true
+      end
+    end
+
+    context "setting not to show the title" do
+      before(:each) do
+        helper.title title, false
+      end
+
+      it "sets the title" do
+        helper.title.should == title
+      end
+
+      it "sets it doesn't show the title" do
+        helper.show_title?.should be_false
+      end
+
+      it "doesn't modify show_title called with no arguments" do
+        helper.title
+        helper.show_title?.should be_false
+      end
+
+    end
+  end
 end 
