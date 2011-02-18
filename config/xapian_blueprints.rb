fcb779912f59d5e83329aa82c5d83ae9a2fb20dd
Javier Martín <elretirao@elretirao.net>
2011-02-18 01:41:46 +0100

    No incluimos borradores en los resultados de búsqueda.

--- config/xapian_blueprints.rb
+++ config/xapian_blueprints.rb
@@ -3,4 +3,5 @@ XapianDb::DocumentBlueprint.setup(Pagina) do |blueprint|
   blueprint.attribute :cuerpo, weight: 4
   blueprint.index :titulo_cajas, weight: 3
   blueprint.index :cuerpo_cajas, weight: 1
+  blueprint.ignore_if { borrador? }
 end
