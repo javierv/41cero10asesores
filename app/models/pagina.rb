b454a9bfae24b2a16d92e841875859985c37214c
Javier Martín <elretirao@elretirao.net>
2011-02-18 01:47:18 +0100

    No precargamos Pagina al arrancar Rails con Xapian.
    Esto influía con spork. De momento, la solución rápida es pasar la
    configuración al modelo.

--- app/models/pagina.rb
+++ app/models/pagina.rb
@@ -26,6 +26,14 @@ class Pagina < ActiveRecord::Base
     ascii_approximation_options:  :spanish,
     reserved_words:               ["search"]
 
+  XapianDb::DocumentBlueprint.setup(Pagina) do |blueprint|
+    blueprint.attribute :titulo, weight: 10
+    blueprint.attribute :cuerpo, weight: 4
+    blueprint.index :titulo_cajas, weight: 3
+    blueprint.index :cuerpo_cajas, weight: 1
+    blueprint.ignore_if { borrador? }
+  end
+
   def self.per_page
     15
   end
