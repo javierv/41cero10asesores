19c2563669a5a8ac6c7c35858caac1a617376ab1
Javier Martín <elretirao@elretirao.net>
2011-02-21 19:01:44 +0100

    Extracción a método y tests de la expresión regular.

--- app/helpers/textile_helper.rb
+++ app/helpers/textile_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 module GoogleMapTag
   def gm(options)
-    data = options[:text].match /(?<lat>.+),(?<long>[^\(]+)(\((?<titulo>.+)\))?/
+    data = extract_coordinates(options[:text])
 
     html = %Q{<div class="google_map" data-latitud="#{data[:lat]}" } +
            %Q{data-longitud="#{data[:long]}"}
@@ -11,6 +11,10 @@ module GoogleMapTag
     end
 
     html + "></div>"
+  end
+
+  def extract_coordinates(text)
+    text.match /(?<lat>.+),(?<long>[^\(]+)(\((?<titulo>.+)\))?/
   end
 end
 RedCloth::Formatters::HTML.send(:include, GoogleMapTag)
--- spec/helpers/textile_helper_spec.rb
+++ spec/helpers/textile_helper_spec.rb
@@ -1,7 +1,31 @@
 # encoding: utf-8
 require 'spec_helper'
 
+class GoogleMapTagTest
+  include GoogleMapTag
+end
+
 describe TextileHelper do
+  describe "extract_coordinates" do
+    let(:block) do
+      lambda { |text| GoogleMapTagTest.new.extract_coordinates(text) }
+    end
+
+    it "permite no poner el título" do
+      data = block["-5.98,4.33"]
+      data[:lat].should == "-5.98"
+      data[:long].should == "4.33"
+      data[:titulo].should be_nil
+    end
+
+    it "permite poner el título" do
+      data = block["-5.98,4.33(Título)"]
+      data[:lat].should == "-5.98"
+      data[:long].should == "4.33"
+      data[:titulo].should == "Título"
+    end
+  end
+
   describe "google maps" do
     let(:html) do
       %Q{<div class="google_map" data-latitud="37.34" } +
