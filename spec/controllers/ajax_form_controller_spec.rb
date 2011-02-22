8400cc762b9887a40f2ed4afca5337e0da8b2efd
Javier Martín <elretirao@elretirao.net>
2011-02-22 20:00:47 +0100

    Simplificación de sintaxis.

--- spec/controllers/ajax_form_controller_spec.rb
+++ spec/controllers/ajax_form_controller_spec.rb
@@ -4,15 +4,13 @@ require 'spec_helper'
 
 describe AjaxFormController do
   include Rails.application.routes.url_helpers
+
   def default_url_options
     { only_path: true }
   end
 
   let(:url) { url_for controller: :paginas, action: :index }
-
-  before(:each) do
-    authenticate_usuario
-  end
+  before(:each) { authenticate_usuario }
 
   context 'pidiendo con la URL del índice' do
     before(:each) do
