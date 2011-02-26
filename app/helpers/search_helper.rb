e066d756fbf90589265efedb5daf084bf91c6f9a
Javier Martín <elretirao@elretirao.net>
2011-02-26 03:19:00 +0100

    Añadimos posibilidad de términos entre comillas.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,6 +2,11 @@
 
 module SearchHelper
   def search_highlight(text, term)
-    highlight text, term.split, highlighter: '<strong class="searched_term">\1</strong>'
+    highlight text, search_split(term), highlighter: '<strong class="searched_term">\1</strong>'
+  end
+
+private
+  def search_split(term)
+    term.scan(/"[^"]+"|\S+/).map { |word| word.delete('"') }
   end
 end
--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -12,8 +12,14 @@ describe SearchHelper do
     end
 
     it "resalta varios términos" do
-      search_highlight(text, "lorem ipsum").should ==
-        %q{<strong class="searched_term">ipsum</strong> <strong class="searched_term">lorem</strong> y toma ya}
+      search_highlight(text, "ipsum lorem").should ==
+        %q{<strong class="searched_term">ipsum</strong> } +
+        %q{<strong class="searched_term">lorem</strong> y toma ya}
+    end
+
+    it "resalta como un término entre comillas" do
+      search_highlight(text, %q{"ipsum lorem"}).should ==
+        %q{<strong class="searched_term">ipsum lorem</strong> y toma ya}
     end
   end
 end
