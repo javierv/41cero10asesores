c109215755ef9e436d5210def5fcbf6ed6681adf
Javier Martín <elretirao@elretirao.net>
2011-02-28 22:27:46 +0100

    Tests de borrar y deshacer borrar.
    Lo que no consigo entender es por qué funciona esto sin javascript :S.

---
+++ spec/acceptance/borrar_paginas_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature "Borrar Páginas", %q{
+  Para quitar información vieja
+  Como administrador
+  Quiero borrar páginas y recuperarlas
+} do
+
+  background do
+    crea_paginas_con_titulos %w(Primera Segunda Tercera)
+    login
+  end
+
+  scenario "Borrar una página y deshacer" do
+    visit paginas_path
+    page.should have_content "Primera"
+
+    click_on "Borrar"
+    page.should have_success(text: "se borró")
+    page.should have_no_content "Primera"
+
+    click_on "Deshacer"
+    page.should have_success(text: "recuperada")
+    page.should have_content "Primera"
+  end
+end
