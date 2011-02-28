f7c60c875eee17604d9f8b97fffe4ab2edf3d266
Javier Martín <elretirao@elretirao.net>
2011-02-28 22:55:57 +0100

    Tests de recuperar páginas borradas.

--- spec/acceptance/borrar_paginas_spec.rb
+++ spec/acceptance/borrar_paginas_spec.rb
@@ -28,4 +28,26 @@ feature "Borrar Páginas", %q{
     page.should have_content "Primera"
     page.should have_content "Segunda"
   end
+
+  scenario "Recuperar una página borrada", js: true do
+    visit paginas_path
+    click_on "Borrar"
+    page.should have_no_content "Primera"
+    click_on "Borrar"
+    page.should have_no_content "Segunda"
+
+    visit deleted_path
+    page.should have_content "Primera"
+    page.should have_content "Segunda"
+    page.should have_no_content "Tercera"
+
+    click_on "Recuperar"
+    current_path.should == paginas_path
+    page.should have_content "Segunda"
+    page.should have_no_content "Primera"
+
+    visit deleted_path
+    page.should have_content "Primera"
+    page.should have_no_content "Segunda"
+  end
 end
