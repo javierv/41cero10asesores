fe49802f32b711259134be94c76fcb188443232f
Javier Martín <elretirao@elretirao.net>
2011-03-05 23:28:01 +0100

    Validación del email de clientes.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,10 @@
+# encoding: utf-8
+
 class Cliente < ActiveRecord::Base
   attr_accessible :nombre, :email
-  validates :nombre, :presence => true
-  validates :email,  :presence => true
+
+  validates :nombre, presence: true
+  validates :email,  presence: true, email: true
+
   display_name :nombre
 end
--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe Cliente do
@@ -6,4 +8,6 @@ describe Cliente do
 
   it { should validate_presence_of(:email) }
   it { should allow_mass_assignment_of(:email) }
+  it { should validate_format_of(:email).with("elretirao@elretirao.net") }
+  it { should validate_format_of(:email).not_with("ddd").with_message(/válido/) }
 end
