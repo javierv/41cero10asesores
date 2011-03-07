1efe6b80f9fb7d41e6fbcf9c19839f1c27902b09
Javier Martín <elretirao@elretirao.net>
2011-03-07 16:57:01 +0100

    Refactorización de test.
    Tenía ganas de usar "subject" con RSpec.

--- spec/mailers/boletin_mailer_spec.rb
+++ spec/mailers/boletin_mailer_spec.rb
@@ -4,19 +4,14 @@ require "spec_helper"
 
 describe BoletinMailer do
   describe "envío boletín" do
-    let(:email) do
+    subject do
       BoletinMailer.envio Factory(:boletin,
         destinatarios: "Pedro <pedro@calesur.es>,Juan <juan@calesur.es>",
         archivo: File.new("spec/images/example.pdf"))
     end
 
-    it "envía a los clientes con copia oculta" do
-      email.bcc.should == %w(pedro@calesur.es juan@calesur.es)
-    end 
-
-    it "adjunta un fichero" do
-      email.attachments.should have(1).item
-      email.attachments[0].filename.should == "example.pdf"
-    end
+    its(:bcc) { should == %w(pedro@calesur.es juan@calesur.es) }
+    its(:attachments) { should have(1).item }
+    its("attachments.first.filename") { should == "example.pdf" }
   end
 end
