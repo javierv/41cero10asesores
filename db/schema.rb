6ae6b0b716837d55c8cc80c4205d9a173ceb18c3
Javier Martín <elretirao@elretirao.net>
2011-03-07 17:15:06 +0100

    Indicación de si un boletín se ha enviado o no.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110307151315) do
+ActiveRecord::Schema.define(:version => 20110307160640) do
 
   create_table "boletines", :force => true do |t|
     t.string   "archivo_uid"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110307151315) do
     t.datetime "updated_at"
     t.string   "titulo"
     t.text     "destinatarios"
+    t.boolean  "enviado"
   end
 
   create_table "cajas", :force => true do |t|
--- spec/models/boletin_spec.rb
+++ spec/models/boletin_spec.rb
@@ -4,4 +4,10 @@ require 'spec_helper'
 
 describe Boletin do
   it { should validate_presence_of(:archivo) }
+
+  describe "enviado" do
+    it "es falso por defecto" do
+      Boletin.new.enviado?.should be_false
+    end
+  end
 end
