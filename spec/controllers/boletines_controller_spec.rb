8db802fda04f46c953aca758de0a61bc1b049702
Javier Martín <elretirao@elretirao.net>
2011-03-07 21:01:33 +0100

    Errata.

--- spec/controllers/boletines_controller_spec.rb
+++ spec/controllers/boletines_controller_spec.rb
@@ -70,7 +70,7 @@ describe BoletinesController do
   describe "email" do
     before(:each) do
       valida_siempre(Boletin)
-      put :email, id: @boletin.to_param, boletin: {emails: "fulano@calesur.com"}
+      put :email, id: @boletin.to_param, boletin: {clientes: ["Fulano <fulano@calesur.com>"]}
     end
 
     it do
