57fb2f515ea4c469d514d1dd58a3fd53a77a511f
Javier Martín <javi@elretirao.net>
2011-11-09 16:50:36 +0100

    La BD de Redis depende del entorno en que se ejecute la aplicación.
    Si no, los tests sobreescribirían la BD de producción :-).

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,9 @@
-TRANSLATION_STORE = Translation.new
+db_number = if Rails.env.test?
+              "2"
+            elsif Rails.env.production?
+              "0"
+            else
+              "1"
+            end
+TRANSLATION_STORE = Translation.new db: db_number
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(TRANSLATION_STORE), I18n.backend)
