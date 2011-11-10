c4006eda3ee4958401eb433320fc48e9ea1e085e
Javier Martín <javi@elretirao.net>
2011-11-10 03:19:15 +0100

    Integramos foreman con inploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,12 @@ application = "calesur"
 repository = '/home/elretirao/git/calesur'
 hosts = ['elretirao.net']
 path = '/home/elretirao'
+release_path = "#{path}/#{application}"
 user = 'elretirao'
 
 before_restarting_server do
   rake "redis:import_yaml"
   rake "assets:precompile"
+  run "cd #{release_path} && sudo foreman export upstart /etc/init -a #{application} -c web=3 -u #{user} -l #{release_path}/log/foreman"
+  run "sudo start #{application} || sudo restart #{application}"
 end
