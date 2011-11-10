2cfab4185cce0484222f37ced773ddaf13fa3c68
Javier Martín <javi@elretirao.net>
2011-11-10 03:06:21 +0100

    Tareas no automáticas para inploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,8 @@ repository = '/home/elretirao/git/calesur'
 hosts = ['elretirao.net']
 path = '/home/elretirao'
 user = 'elretirao'
+
+before_restarting_server do
+  rake "redis:import_yaml"
+  rake "assets:precompile"
+end
