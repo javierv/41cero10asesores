44762d6f0868cd586000cd63ca1931f8836f6683
Javier Martín <javi@elretirao.net>
2012-02-06 05:56:46 +0100

    Fuera métodos obsoletos.
    Al quitar los hacks que tenía de javascripts en la previsualización, esto ya no hace falta.

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -3,45 +3,6 @@
 require 'spec_helper'
 
 describe LayoutHelper do
-  describe "assets" do
-    context "adding a javascript" do
-      before(:each) { helper.javascript 'application' }
-
-      it 'adds a javascript to the list' do
-        helper.javascripts.should include('application')
-      end
-    end
-    context "adding several javascripts" do
-      let(:javascripts) { ['jquery', 'rails', 'application'] }
-      before(:each) { helper.javascript *javascripts }
-
-      it 'adds all the javascripts' do
-        helper.javascripts.should == javascripts
-      end
-
-      it 'allows to add another javascript' do
-        helper.javascript 'jquery.ui'
-        helper.javascripts.should == javascripts + ['jquery.ui']
-      end
-
-      it "doesn't add the same javascripts twice" do
-        helper.javascript 'jquery'
-        helper.javascripts.should_not == javascripts + ['jquery']
-      end
-    end
-  end
-
-  describe "including assets" do
-    context "javascripts" do
-      before(:each) { helper.javascript 'form', 'diff' }
-
-      it "includes javascript tags" do
-        helper.include_javascripts.should have_selector('script[src*=form]')
-        helper.include_javascripts.should have_selector('script[src*=diff]')
-      end
-    end
-  end
-
   describe "#title" do
     let(:title) { 'Mi título' }
 
