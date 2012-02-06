ad22e7c25e3b7ffa26a7685ce9a2cfa3265bec28
Javier Martín <javi@elretirao.net>
2012-02-06 06:13:44 +0100

    Fuera comentario obsoleto.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 
 module LayoutHelper
-  # Compatible with ryanb's
-  # It doesn't mess up with content_for, but the code is quite ugly
   def title(page_title = @page_title, show_title = true)
     @show_title = show_title unless defined?(@show_title)
     @page_title = page_title
