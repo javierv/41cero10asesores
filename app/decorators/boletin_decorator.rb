7a68ef201365317f75334d9a222dc337848a15df
Javier Martín <javi@elretirao.net>
2012-02-11 20:23:50 +0100

    Checkbox de enviar boletín, al decorador.
    Lo cierto es que este método no me gusta en ningún sitio, pero en la vista me hace especial daño.

--- app/decorators/boletin_decorator.rb
+++ app/decorators/boletin_decorator.rb
@@ -7,6 +7,16 @@ class BoletinDecorator < ApplicationDecorator
     archivo_name.force_encoding("utf-8")
   end
 
+  def destinatarios_check_boxes
+    model.clientes.map.with_index do |cliente, index|
+      id = "boletin_destinatarios_#{index}"
+      h.content_tag :div, class: "input boolean" do
+        h.check_box_tag("boletin[clientes][]", cliente, true, id: id) +
+        h.label_tag(id, cliente)
+      end
+    end.join.html_safe
+  end
+
 private
   def acciones
     [:show, :edit, :destroy, (:enviar unless model.enviado?)].compact
