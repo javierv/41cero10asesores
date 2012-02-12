c772b9e1e121a894385461efd21c8047af88177d
Javier Martín <javi@elretirao.net>
2012-02-12 01:17:24 +0100

    Nuevamente volvemos a ofrecer la opción de no mostrar el título.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -5,6 +5,14 @@ module LayoutHelper
     @title ||= I18n.translate! page_title_path, {resource: @resource}.merge(variables)
   end
 
+  def show_title?
+    @show_title = true if @show_title.nil?
+  end
+
+  def dont_show_title
+    @show_title = false
+  end
+
 private
   def page_title_path
     "page_titles.#{params[:controller]}.#{params[:action]}"
