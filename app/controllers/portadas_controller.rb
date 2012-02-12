5d5036391eee96493cb3e740b410d0830fc279f1
Javier Martín <javi@elretirao.net>
2012-02-12 06:05:08 +0100

    Fuera variables de instancia en portadas.

--- app/controllers/portadas_controller.rb
+++ app/controllers/portadas_controller.rb
@@ -2,34 +2,41 @@
 
 class PortadasController < ApplicationController
   respond_to :html
-  before_filter :find_portada, except: :principal
   before_filter :update_portada, only: [:update, :create]
 
   public_actions :principal
 
   def principal
-    @pagina = PaginaDecorator.decorate Portada.first!.pagina
-    respond_with @pagina, template: "paginas/show"
+    respond_with pagina, template: "paginas/show"
   end
 
   def new
-    @paginas = Pagina.publicadas
   end
 
   def update
-    respond_with @portada, location: new_portada_path
+    respond_with portada, location: new_portada_path
   end
 
   def create
-    respond_with @portada, location: new_portada_path
+    respond_with portada, location: new_portada_path
   end
 
 private
-  def find_portada
-    @portada = Portada.portada
+  def portada
+    @portada ||= Portada.portada
+  end
+
+  def pagina
+    @pagina ||= PaginaDecorator.decorate Portada.first!.pagina 
   end
 
   def update_portada
-    @portada.update_attributes params[:portada]
+    portada.update_attributes params[:portada]
   end
+
+  def paginas
+    Pagina.publicadas
+  end
+
+  helper_method :portada, :pagina, :paginas
 end
