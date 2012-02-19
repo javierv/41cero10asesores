1f57e3a038c5c05440121d312d1f1d8764d65014
Javier Martín <javi@elretirao.net>
2012-02-19 01:12:18 +0100

    Application.js se utiliza para notas sobre la licencia.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,23 @@
+/*!
+ * This is a compressed version of the code, for performance reasons.
+ * You can find the source code, under a free software license, at:
+ *
+ * http://github.com/javierv/41cero10asesores
+ *
+ * Please note that the code at:
+ *
+ * https://github.com/javierv/41cero10asesores/tree/master/app/assets/javascripts
+ *
+ * has been authored by 41cero10 Asesores and Javier Martín and it's distributed by
+ * the free software license indicated there. However, the code at:
+ *
+ * https://github.com/javierv/41cero10asesores/tree/master/vendor/assets/javascripts
+ *
+ * was written by other authors, and you can find their licenses (all of them are free
+ * software licenses) in their source code.
+ *
+ */
+//= require_self
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
@@ -12,5 +32,3 @@
 //= require jquery.autosave_form
 //= require jquery.paginator
 //= require_tree .
-
-$.ajaxSetup({dataType: 'text'})
